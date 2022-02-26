48ff9c1416937b02e096e9e85b239e3bb828484d
daeyeub <97010621+daeyeub@users.noreply.github.com>
2022-02-26 19:03:49 +0900

    2022-02-26 pm7

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
-import express from "express";
+import express, { Router } from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT=7000;
 
 const app=express();
 const logger = morgan("dev");
 
-const home = (req, res) => {
-    return res.send("hello"); 
-};
-
-const login = (req, res) => {
-    return res.send("login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos",videoRouter);
+
+
 
 
 const handleListening=()=>{
