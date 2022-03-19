9d506af96744dfacb9a74215a284ae4c3b0acd6d
daeyeub <97010621+daeyeub@users.noreply.github.com>
2022-03-20 01:32:55 +0900

    #6.17 Exceptions and Validation(2022.03.20)

--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import "./db";
 import "./models/Video";
 import app from "./server";
 
-const PORT=4500;
+const PORT=7000;
 
 const handleListening=()=>{
     console.log(`✅ Server listening on port http://localhost:${PORT}`);
