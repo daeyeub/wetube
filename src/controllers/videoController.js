ba63b88d550b8ec5c5f0bff512d661da40d68bab
daeyeub <97010621+daeyeub@users.noreply.github.com>
2022-03-19 17:07:32 +0900

    2022-03-19 ~#6.16

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,8 @@
 import Video from "../models/Video";
 
 export const home =async(req, res) => {
-  const videos=await video.find({})
+  const videos=await Video.find({})
+  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos});
 };
 export const watch = (req, res) => {
@@ -22,7 +23,18 @@ export const getUpload=(req,res)=>{
   return res.render("upload",{pageTitle:"Upload Video"});
 };
 
-export const postUpload=(req, res)=>{
-  const {title}=req.body;
+export const postUpload=async(req, res)=>{
+  const {title, description, hashtags}=req.body;
+  const video=new Video({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags:hashtags.split(",").map((word)=>`#${word}`),
+    meta:{
+      views:0,
+      rating:0,
+    },
+  })
+  await video.save();
   return res.redirect("/");
 };
--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import "./db";
 import "./models/Video";
 import app from "./server";
 
-const PORT=7000;
+const PORT=4500;
 
 const handleListening=()=>{
     console.log(`✅ Server listening on port http://localhost:${PORT}`);
